e478a33604c4f1a67ec7d85e35621bcf141ad8cb
Blabla <alissonfigueral@gmail.com>
2024-04-14 00:54:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Desafio
+ Aprendendo a programar
---
+++ script.js
@@ -0,0 +1,6 @@
+var anim = document.getElementById("a")
+
+document.getElementById("char").addEventListener("click", function() {
+    document.getElementById("alt").style.animation = "name 1s";
+  });
+  
